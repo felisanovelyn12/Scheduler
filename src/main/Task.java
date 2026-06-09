222ad52dde7c66f58fa44e6edc53486f2dd72ba0
felisanovelyn <fnovelyn@students.cs.ubc.ca>
2026-06-09 04:16:02 -0700

    updated Task.java

--- src/main/Task.java
+++ src/main/Task.java
@@ -6,17 +6,11 @@ public class Task {
     private String taskName;
     private LocalDateTime deadline;
     private int estimatedTime;
-    private Priority priority;
 
-    public enum Priority {
-        HIGH, MEDIUM, LOW;
-    }
-
-    public Task(String taskName, LocalDateTime deadline, int estimatedTime, Priority priority) {
+    public Task(String taskName, LocalDateTime deadline, int estimatedTime) {
         this.taskName = taskName;
         this.deadline = deadline;
         this.estimatedTime = estimatedTime;
-        this.priority = priority;
     }
 
     public String getTaskName() {
@@ -41,13 +35,5 @@ public class Task {
 
     public void changeEstimatedTime(int estimatedTime) {
         this.estimatedTime = estimatedTime;
-    }
-
-    public Priority getPriority() {
-        return priority;
-    }
-
-    public void changePriority(Priority priority) {
-        this.priority = priority;
     }
 }
